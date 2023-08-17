b74c9e0ca22a4da0c2b841c8fa3fcd4ffab124a7
Kang Y <kang.times@outlook.com>
2023-08-17 22:19:58 +0800

    v1.2 add led.

---
+++ 06.LED/Led-v01/Led-v01.ino
@@ -0,0 +1,52 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  for (int n=2;n<=9;n++)
+    {
+      pinMode(n,OUTPUT);
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,1);
+
+  int n0[8]={0,0,0,0,0,0,1,1};
+  int z=0;
+  for(int x=2;x<=9;x++)
+  {
+    digitalWrite(x,n0[z]);
+    z++;
+    if(z>=9)
+    {
+      z=0;
+    }
+    delay(1000);
+  }
+  
+  int n1[8]={1,0,0,1,1,1,1,1};
+   z=0;
+  for(int x=2;x<=9;x++)
+  {
+    digitalWrite(x,n0[z]);
+    z++;
+    if(z>=9)
+    {
+      z=0;
+    }
+    delay(1000);
+  }
+
+  int n2[8]={0,0,1,0,0,1,0,1};
+  z=0;
+  for(int x=2;x<=9;x++)
+  {
+    digitalWrite(x,n0[z]);
+    z++;
+    if(z>=9)
+    {
+      z=0;
+    }
+    delay(1000);
+  }
+}
